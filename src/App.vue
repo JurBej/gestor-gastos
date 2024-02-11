bd7612160ae811910b3b222080e00709b4116385
Ángel Jurado Bejarano <ajurbej2707@g.educaand.es>
2024-02-11 17:29:30 +0100

    completed

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,73 @@ import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
+
+import {useToast} from 'vue-toastification'
+const toast = useToast()
+
+import {computed, ref, onMounted} from 'vue';
+/* const transactions = ref ([
+    {id: 1, text: 'Flower', amount: -19.99},
+    {id: 2, text: 'Salary', amount: 219.99},
+    {id: 3, text: 'Book', amount: -109.99},
+    {id: 4, text: 'Camera', amount: 419.99},
+]) */
+
+const transactions = ref([])
+
+onMounted(() => {
+  const savedTransaction = JSON.parse(localStorage.getItem('transactions'))
+  if (savedTransaction) {
+    transactions.value = savedTransaction
+  }
+})
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0).toFixed(2)
+})
+
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount >0 )
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0).toFixed(2)
+})
+
+const expenses = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount < 0 )
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0).toFixed(2)
+})
+
+const handleTransactionSubmitted = (transactionData) => {
+  console.log(transactionData);
+  transactions.value.push({
+    id: generatedUniqueID(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+  saveTransactionToLocalStorage()
+  toast.success('Transaction added')
+}
+
+const generatedUniqueID = () => {
+  return Math.floor((Math.random() * 1000000))
+}
+
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+  saveTransactionToLocalStorage()
+  toast.success('Transaction deleted')
+}
+
+const saveTransactionToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
 </script>
 
 <template>
@@ -11,10 +78,10 @@ import AddTransaction from './components/AddTransaction.vue';
 
   <main>
     <div class="container">
-      <Balance></Balance>
-      <IncomeExpenses></IncomeExpenses>
-      <TransactionList></TransactionList>
-      <AddTransaction></AddTransaction>
+      <Balance :total="+total"></Balance>
+      <IncomeExpenses :income="+income" :expenses="+expenses"></IncomeExpenses>
+      <TransactionList :transactions="transactions" @deleteTransaction="handleTransactionDeleted"></TransactionList>
+      <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
     </div>
   </main>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,18 +1,25 @@
 <script setup>
-import {ref} from 'vue';
-const transactions = ref ([
-    {id: 1, text: 'Flower', amount: -19.99},
-    {id: 2, text: 'Salary', amount: 219.99},
-    {id: 3, text: 'Book', amount: -109.99},
-    {id: 4, text: 'Camera', amount: 419.99},
-])
+import { defineProps } from 'vue';
+
+const emit = defineEmits(['deleteTransaction'])
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
+
+const deleteTransaction = (id) => {
+    emit('deleteTransaction', id)
+}
 </script>
 
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
         <li v-for="(transaction) in transactions" :key="transaction.id" :class="transaction.amount<0 ? 'minus' : 'plus'">
-            {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
+            {{ transaction.text }} <span>{{ transaction.amount }}</span><button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
         <!-- <li class="plus">
             Cash <span>+$200</span><button class="delete-btn">x</button>
